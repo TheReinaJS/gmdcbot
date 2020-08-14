aaa27bdeb50f34c0c6a438bdc14b8cf6b9aac373
Glitch (nostalgic-aboard-bedbug) <none>
2020-08-14 04:02:51 +0000

    ✍️🔮 Checkpoint ./komutlar/kodlar.js:16385866/4852

--- komutlar/kodlar.js
+++ komutlar/kodlar.js
@@ -1,22 +1,23 @@
-const Discord = require('discord.js');
-exports.run = function(client, message, args) {
+const Discord = require('discord.js')
 
-message.author.send(new Discord.MessageEmbed()
-  .setDescription('**KİNSTA KOD ALMA BİLGİ**')
-  .addField(`** **`, `\`Ücretsiz\` **__JavaScript__**`)
-  .addField(`** **`, `\`Ücretsiz\`  **</Alt Yapı/>**`)
-  .addField(`** **`, `\`30 İnvite\`  **</Premium/>**`)
-  .setColor("BLACK"))
+
+exports.run = (client, message, args) => {
   
-};
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("Bu Komutu Kullanmaya Yetkin Yok!!").then(m => m.delete(5000));
+  
+var user = message.guild.member(message.mentions.users.cache.first() || message.guild.members.cache.get(args[0]));
+  
+message.guild.setName(args.join(" ")).then(() => message.channel.cache.send(`Sunucunun İsmi \`${args.join(" ")}\` Yapıldı`)).catch(_ => message.channel.cache.send(`Yeterli Yetkim YOK!`))
+}
 
-exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: [],
-  permLevel: 0
-};
+exports.conf = { 
+enabled: true, guildOnly: true, 
+permlevel: 3, 
+aliases: ["guild-name-change"]
+}
 
 exports.help = {
-  name: 'kodlar',
-};
+name: "sunucu-ismi-değiştir", 
+description: "sunucu ismi değiştirir", 
+usage: "sunucu-ismi-değiştir <yeni isim>"
+}
