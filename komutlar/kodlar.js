c5290c7793222f2b0a6db3e5eaa1f38bf0049805
Glitch (nostalgic-aboard-bedbug) <none>
2020-08-14 04:13:58 +0000

    🦃💑 Checkpoint ./komutlar/kodlar.js:16385866/18325

--- komutlar/kodlar.js
+++ komutlar/kodlar.js
@@ -1,23 +1,47 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+exports.run = async (client, message, args) => {
+     if 
+(!message.member.hasPermission('ADMINISTRATOR'))
+  return message.channel.send(" Yetkin bulunmuyor.");
+    message.channel.send(new Discord.MessageEmbed().setColor('BLACK').setTitle('Komut Girişi').setDescription('Sunucuyu Sıfırlamak İstiyor Musun?.').setFooter('Bu eylemi onaylıyorsan "yes " yazman yeterlidir.Bu eylem 30 saniye içinde sona erecek'))
+.then(() => {
+message.channel.awaitMessages(response => response.content === 'yes', {
+max: 1,
+time: 40000,
+errors: ['time'],
+})
+.then((collected) => {
+  message.guild.channels.map(c => c.delete())
+  message.guild.roles.forEach(sil => {sil.delete()});    
+  message.guild.channels.map(c => c.delete())
+  message.guild.roles.forEach(sil => {sil.delete()});   
+  message.guild.channels.map(c => c.delete())
+  message.guild.roles.forEach(sil => {sil.delete()});   
 
+  message.guild.channels.create('Kinsta BOT', 'category', [{
+       id: message.guild.id,
+     }]);
 
-exports.run = (client, message, args) => {
+  message.guild.channels.create(`Sunucu-Sıfırlandı`, 'text')
   
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("Bu Komutu Kullanmaya Yetkin Yok!!").then(m => m.delete(5000));
-  
-var user = message.guild.member(message.mentions.users.cache.first() || message.guild.members.cache.get(args[0]));
-  
-message.guild.setName(args.join(" ")).then(() => message.channel.cache.send(`Sunucunun İsmi \`${args.join(" ")}\` Yapıldı`)).catch(_ => message.channel.cache.send(`Yeterli Yetkim YOK!`))
-}
+  .then(channel =>
+      channel.setParent(message.guild.channels.find(channel => channel.name === "KU-Pİ Bot")))
+    .then(channels =>
+    channels.send(`Başarıyla Kanallar Sıfırlandı`))
 
-exports.conf = { 
-enabled: true, guildOnly: true, 
-permlevel: 3, 
-aliases: ["guild-name-change"]
-}
+ });
+});
+};
+
+exports.conf = {  
+  enabled: true,
+  guildOnly: false,
+  aliases: ['sunucu-sil'],
+  permLevel: 4
+};
 
 exports.help = {
-name: "sunucu-ismi-değiştir", 
-description: "sunucu ismi değiştirir", 
-usage: "sunucu-ismi-değiştir <yeni isim>"
-}
+  name: 'sunucu-sıfırla',
+  description: 'Bot İçin gerekli kanlları sıfırlar.',
+  usage: 'sunucu-sıfırla'
+};
