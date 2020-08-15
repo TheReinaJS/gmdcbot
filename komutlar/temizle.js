3c94cffeb99be1c58f06411a5dcf190735be994e
Glitch (nostalgic-aboard-bedbug) <none>
2020-08-15 12:23:49 +0000

    🏞🏇 Checkpoint ./komutlar/afk.js:19118297/1823 ./index.js:19118297/544 ./komutlar/temizle.js:19118297/2599

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -3,28 +3,47 @@ const Discord = require('discord.js')
 exports.run = async (client, message, args) => { 
      
   if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(`Bu komutu kullanabilmek için **Mesajları Yönet** yetkisine sahip olmalısın!`);
-  if (!args[0] || isNaN(args[0])) return message.reply(`Temizlenecek mesaj miktarını belirtmelisin!`);
+  if (!args[0] || isNaN(args[0])) {
+    
+  const temizle = new Discord.MessageEmbed()  
+  
+  .setDescription(`Temizlenecek Mesaj Miktarını Belirtmelisin!`)
+  .setColor("RED")
+  
+  return message.channel.send(temizle)
+  }
+  
   message.delete();
+  
   let sayi = Number(args[0]);
   let silinen = 0;
+  
   for (var i = 0; i < (Math.floor(sayi/100)); i++) {
-   message.channel.bulkDelete(100).then(r => silinen+=r.size);
-    sayi = sayi-100;
+    
+  message.channel.bulkDelete(100).then(r => silinen+=r.size);
+    
+  sayi = sayi-100; 
   };
+  
   if (sayi > 0)  message.channel.bulkDelete(sayi).then(r => silinen+=r.size);
-  message.channel.send(`**\`\`${args[0]}\`\` Adet Mesaj Silindi.**`);
- 
-}
+  
+  const sil = new Discord.MessageEmbed()  
+  
+  .setDescription(`**\`\`${args[0]}\`\` Adet Mesaj Silindi.**`)
+  .setColor("GREEN")
+  
+  return message.channel.send(sil)
+  }
 
 exports.conf = {
     enabled: false,
     guildOnly: false,
-    aliases: [],
+    aliases: ['temizle','clear','purge','delete'],
     permLevel: 0,
 }
 
 exports.help = {
     name: 'sil', 
     description: 'Kinda Code & Only V12',
-    usage: 'Kinda Code & Only V12'
+    usage: 'sil'
 }
