f58aa943821322b18f01d3758deecbf7ea777ad9
Glitch (nostalgic-aboard-bedbug) <none>
2020-08-15 13:43:58 +0000

    🙏🌏 Checkpoint ./komutlar/giriş-kanal.js:19118297/3239

--- komutlar/giriş-kanal.js
+++ komutlar/giriş-kanal.js
@@ -12,32 +12,31 @@ const embed = new Discord.RichEmbed()
 .setDescription('**Giriş Kanalını Ayarlamak İçin `Sunucuyu Yönet` İznine Sahip Olmalısın!')
 
 return message.channel.send(embed)
-}  
+}
   
-if (args[0] === 'kapat') {
-  
-db.delete(`${message.guild.id}.hgK`)
-db.delete(`${message.guild.id}.kanal`)
-  
-const embed = new Discord.MessageEmbed() 
-  
-.setDescription(`Giriş Kanalı Başarıyla Sıfırlandı!`)
-.setColor("GREEN")
-  
-return message.channel.send(embed)
-}    
-  
+let kinal = db.fetch(`hgK_${message.guild.id}`)
 if(db.has(`hgK_${message.guild.id}`)) {
   
-const embed = new Discord.MessageEmbed()
+const embed = new Discord.RichEmbed()
 
 .setColor('RED')
-.setDescription(`**Giriş Kanalını Etiketlemedin! \n\nAçmak İçin : \`${ayarlar.prefix}giriş-kanal #kanal\`**`)
+.setDescription(`**Giriş Kanalı <#${kinal}> Olarak Ayarlı! Kapatmak İçin** \`${ayarlar.prefix}giriş-kanal kapat\``)
 
 return message.channel.send(embed)
-};
+}
   
-let kanal = message.mentions.channels.first();  
+let kanal = message.mentions.channels.first();
+  
+if(!kanal) {
+  
+const embed = new Discord.RichEmbed()
+
+.setColor('RED')
+.setDescription(`**Giriş Kanalını Etiketlemedin! \`Doğru kullanım: ${ayarlar.prefix}giriş-kanal #kanal\`**`)
+
+return message.channel.send(embed)
+}
+  
 db.set(`hgK_${message.guild.id}`, kanal.id);
 
 const embed = new Discord.RichEmbed()
@@ -45,18 +44,18 @@ const embed = new Discord.RichEmbed()
 .setColor('GREEN')
 .setDescription(`**Giriş Kanalı ${kanal} Olarak Ayarlandı!**`)   
 
-message.channel.send(embed)  
+message.channel.send(embed)                                                                                                                                      
 }; 
 
 exports.conf = {
 enabled: true,
 guildOnly: false,
-aliases: ['hoşgeldinkanal','hg-kanal','hgkanal','girişkanal','hoşgeldin-kanal'],
+aliases: ['hgkanal','hg-kanal','girişkanal'],
 permLevel: 0
 };
 
 exports.help = {
 name: 'giriş-kanal',
 description: 'Hoşgeldin kanalını ayarlamaya yarar.',
-usage: 'giriş-kanal #kanal'
+usage: 'giriş-kanal'
 };
