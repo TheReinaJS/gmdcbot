63dc9e6ea3d33ccddc06fa6faad8c63026de56fe
Glitch (nostalgic-aboard-bedbug) <none>
2020-08-15 20:00:50 +0000

    🍪🌕 Checkpoint ./komutlar/yardım.js:16385866/1556 ./ayarlar.json:16385866/118

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,5 +5,18 @@ exports.run = async(client, message, args) => {
   const embed = new Discord.MessageEmbed()
   .setColor("BLACK")
   .setTitle('YARDIM MENÜSÜNE HOŞGELDİN ^^')
-  .setDescription('botu kullanırken rollerde en üste almayı unutma.\n``-sil``\nİstediğiniz miktarda mesaj silersiniz\n\n``-avatar``\nAvatar  profilinizi atar.\n\n``-istatistik``\nBot hakkında bilgi verir.\n\n``-davet``\nBotun davet linkini atar\n\n```-everhereengel``\nEveryone Here Engel Sistemini görüntülersiniz.\n\n``-hata`` ')
+  .setDescription('botu kullanırken rollerde en üste almayı unutma.\n``-sil``\nİstediğiniz miktarda mesaj silersiniz\n\n``-avatar``\nAvatar  profilinizi atar.\n\n``-istatistik``\nBot hakkında bilgi verir.\n\n``-davet``\nBotun davet linkini atar\n\n```-everhereengel``\nEveryone Here Engel Sistemini görüntülersiniz.\n\n``-hata``\nHata bildirimi yaparsınız.\n\n``-isim-değiştir``\nBelirlediğiniz kullanıcının ismini değiştirirsiniz.\n\n``-istek``\nİstek bildiriminde bulunursunuz.\n\n``-korona``\nSeçilen ülkü kodunu girerek korona hakkındaki istatistikleri görüntülersiniz.\n\n``-kullanıcıbilgim``\nSizin veya etiketledğiniz kullanıcının bilgilernini gösterir.\n\n``-küfürengel``\nKüfür Engel Sistemini açarsınız.\n\n``-otorol``\nSunucuya yeni gelen üyeye ayarlanan rölü verir.\n\n``-ping``\nBotun pingi hakkında bilgi verir\n\n``-prefix``\nSunucuza özel prefix ayarlarsınız.\n\n``-prefix-sıfırla``\nAyarlanan prefixi sıfırlarsınız.\n\n``-reklamengel``\nReklam engel sistemini açarsınız.\n\n``-reklamtaraması``\nSunucunuzda reklam taramasını yaparsınız.\n\n``-sa-as``\nSa diyen kişiye otomatik as cevabını verir\n\n``-sunucubilgi``\nSunucu hakkında bilgi verir\n\n``-slowmode``\nYavaş mod sistemini çalıştırırsınız.')
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  permLevel: 0,
+  aliases: ['help', 'h', 'y', 'yardım']
+}
+
+exports.help = {
+  name: 'yardım',
+  description: 'Yardım menüsünü gösterir.Kodlayan Alycia Debnam',
+  usage: 'yardım'
 }
