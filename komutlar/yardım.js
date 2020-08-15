d84b8e68fc9578a4d90e93830134e484abf89378
Glitch (nostalgic-aboard-bedbug) <none>
2020-08-15 19:49:25 +0000

    ⭐️🎅 Checkpoint ./komutlar/yardım.js:16385866/829 ./index.js:19118297/2809

---
+++ komutlar/yardım.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+
+exports.run = async(client, message, args) => {
+  
+  const embed = new Discord.MessageEmbed()
+  .setColor("BLACK")
+  .setTitle('YARDIM MENÜSÜNE HOŞGELDİN ^^')
+  .setDescription('botu kullanırken rollerde en üste almayı unutma.\n``-sil``\nİstediğiniz miktarda mesaj silersiniz\n\n``-avatar``\nAvatar  profilinizi atar.\n\n``-istatistik``\nBot hakkında bilgi verir.\n\n``-davet``\nBotun davet linkini atar\n\n```-everhereengel``\nEveryone Here Engel Sistemini görüntülersiniz.\n\n``-hata`` ')
+}
