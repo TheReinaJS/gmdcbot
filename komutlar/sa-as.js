114e0b8fc624a10b24fb38417488fcaba2822b21
Glitch (nostalgic-aboard-bedbug) <none>
2020-08-15 15:16:55 +0000

    🏃🏄 Checkpoint ./komutlar/sa-as.js:19118297/255

--- komutlar/sa-as.js
+++ komutlar/sa-as.js
@@ -3,22 +3,30 @@ const ayarlar = require(`../ayarlar.json`)
 const db = require(`quick.db`);
 
 exports.run = (client, message, args) => {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bu Komutu Kullanmak İçin `MESAJLARI YÖNET` yetkisine sahip olamlısınız!")
-    const Bdgo = args.join(` `);
-    if(!Bdgo) message.channel.send(
-        new Discord.MessageEmbed()
-        .setTitle(`Doğru Kullanım`)
-        .setDescription(`Bu Komutu Çalıştımam İçin __+sa-as aç__ veya __+sa-as kapat__ yazınız!`)
-    .setColor(`RANDOM`)        
+  
+if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bu Komutu Kullanmak İçin `MESAJLARI YÖNET` yetkisine sahip olamlısınız!")
+
+const Bdgo = args.join(` `);
+  
+if(!Bdgo) message.channel.send(
+  
+new Discord.MessageEmbed()
+  
+.setTitle(`Doğru Kullanım`)
+.setDescription(`Sa-as Sistemini Çalıştırmak İçin ; \n**-sa-as aç** Veya **-sa-as kapat** Yazınız!`)
+.setColor(`RED`)        
 )
+  
 if(Bdgo === "aç") {
-    db.set(`sa-as_${message.guild.id}`, `acik`);
-    message.channel.send(
-        new Discord.MessageEmbed()
-        .setTitle(`Başarılı`)
-        .setDescription(`Bundan Sonra __sa__ Yazıldığında Cevap Vereceğim`)
-        .setColor(`RANDOM`)
-    )
+  
+db.set(`sa-as_${message.guild.id}`, `acik`);
+message.channel.send(
+  
+new Discord.MessageEmbed()
+.setTitle(`Başarılı`)
+.setDescription(`Bundan Sonra **sa** Yazıldığında as Vereceğim`)
+.setColor(`RANDOM`)
+)
 }
 
 else if(Bdgo === "kapat") {
