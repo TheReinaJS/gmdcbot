a05fa7256bb6100a6d429cac4d91756474b44170
Glitch (nostalgic-aboard-bedbug) <none>
2020-08-16 06:54:56 +0000

    🏬🎐 Checkpoint ./komutlar/yardım.js:19118297/49 ./komutlar/davet-sistemi.js:19118297/236 ./package.json:19118297/2412 ./komutlar/prefixs.js:19118297/1105 ./komutlar/prefix-sıfırla.js copy:19118297/924

--- komutlar/davet-sistemi.js
+++ komutlar/davet-sistemi.js
@@ -5,7 +5,8 @@ exports.run = function(client, message, args) {
   
 const kinda = new Discord.MessageEmbed() 
                             
-.setDescription("**Bot Davet**", "[Davet Et](BOT DAVET LİNKİ)")
+.setTitle("Kinsta Bot Davet")
+.setDescription("**Botun Davet Linki :** [Davet Et](https://discord.com/oauth2/authorize?client_id=744137158004047882&scope=bot&permissions=8)\n**Botun Destek Sunucusu :** [Katıl](https://discord.gg/Zv9C4ep)")
 .setColor("GREEN")
 
 return message.channel.send(kinda)
