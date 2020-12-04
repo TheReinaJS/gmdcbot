8a827105958581292258d2f89c930551dae85f0a
Glitch (nostalgic-aboard-bedbug) <none>
2020-12-04 15:07:02 +0000

    🎑🎹 Checkpoint ./ayarlar.json:22384402/156 ./events/ready.js:22384402/31 ./komutlar/küfür-engel.js:22384402/410

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = client => {
 }, 8000);
 client.user.setPresence({
         game: {
-            name: `lrowsxrd Moderasyon Altyapı`,
+            name: `Öhöm`,
             type: 'WATCHING'   //WATCHING - İZLİYOR LISTINING - DİNLİYOR
         },
         status: 'online'   //online - Çevrimiçi idle - Boşta
