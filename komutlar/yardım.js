10ff9ebadd781c4cb951fbe50b48129c2d655c76
Glitch (nostalgic-aboard-bedbug) <none>
2020-12-04 18:43:31 +0000

    💫🐲 Checkpoint ./komutlar/sesli-kanal-ayarla.js:22384402/2 ./komutlar/sesli-kanal-gir.js:22384402/2 ./komutlar/yardım.js:22384402/1233

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,11 +5,9 @@ exports.run = (client, message) => {
 const EmbedFwhyCode = new Discord.MessageEmbed()
 
 .setColor("RANDOM")
-.setTitle("**▬▬▬▬▬▬[** ©️ **Daemon** ©️ **]▬▬▬▬▬▬**  \n\n> :floppy_disk: `+token` **Botun tokenini görürsünüz.** \n> :floppy_disk: **Şuanda kullanılan prefix** `+`")
 .setThumbnail("https://cdn.discordapp.com/attachments/784405293139623967/784476446009589800/780535841630060554.gif")
 .setDescription(`
-
-▬▬▬▬▬▬[ :closed_lock_with_key: **Daemon Yardım Menüsü** :closed_lock_with_key: ]▬▬▬▬▬▬
+▬▬▬▬▬▬[ :closed_lock_with_key: **Yrnex Creative・YouTube Yardım Menüsü** :closed_lock_with_key: ]▬▬▬▬▬▬
 
 > **» +aduketçek :** Etiketlediğiniz kişiye aduket çekersiniz.
 > **» +sa-as :** Bot sa yazdığınızda cevap verir.
@@ -20,24 +18,17 @@ const EmbedFwhyCode = new Discord.MessageEmbed()
 > **» +reklam-taraması :** Bot oynuyor kısmında reklam olan kişilileri belirler.
 > **» +kanal-kilit :** Bot komutu kullandığınız kanalı kilitler.
 > **» +kilit-aç :** Bot komutu kullandığınız kanalı kilitini açar.
-> **» +kaçcm :** Bot malafat uzunluğunuzu söyler.
-> **» +kralol :** Kral olmanıza yarar.
+> **» +sor :** Bota Soru Sorarsınız.
+> **» +yetkilerim:** Yetkilerinizi Görürsünüz.
 > **» +sil :** Bot belirttiğiniz miktarda mesaj siler.
 > **» +av :** Bot avatarınızı veya etiketlediğiniz kişininkini gösterir.
 > **» +afk :** Bot afk olduğunuzu belirtir.
-> **» +öp :** Etiketlediğiniz kişiyi öpmenize yarar.
+> **» +ping :** Botun Gecikme Süresini Görürsünüz.
+> **» +şut-çek :** Şut Çekersiniz İşte a.q.
+> **» +botbilgi :** Botun Bilgilerini Öğrenirsiniz.
 > **» +ever-here-engel :** Bot sunucunuzda ever-here atılmasını engeller.
-> **» +yavaş-mod :** Bot komutu kullandığınız kanalda yavaş mod uygular.
-> **» +fal :** Bot falınıza bakar.
-> **» +token :** Botun tokenini görürsünüz.
-> **» +prefix :** Bot belirlediğiniz prefix ile çalışır NOT:(Yardım Sekmesi Değişmez).
-> **» +davet :** Bot davet linklerini gösterir.
-
-**▬▬▬▬▬▬▬[** :gear: **Bilgilendirme** :gear: **]▬▬▬▬▬▬▬**
-
-> :dizzy: **Fikirleriniz** **değerlidir, Belirtmekten asla çekinmeyin!**
-> :round_pushpin: **Botun Gecikme Süresini Öğrenmek İçin +ping**
-> :airplane: **Aktif discord.js sürümüm: v12.2.0**`)
+> **» +slowmode :** Bot komutu kullandığınız kanalda yavaş mod uygular.`)
+.setImage('https://media.discordapp.net/attachments/767428424251211796/769168676841848862/standard_1.gif')
 
 
  
