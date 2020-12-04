31a1ded9cbe348d2e06287a2b1ef17c907dd59cd
Glitch (nostalgic-aboard-bedbug) <none>
2020-12-04 15:29:59 +0000

    🍳🎮 Checkpoint ./komutlar/küfür-engel.js:21521021/1:22384402/6058

--- komutlar/küfür-engel.js
+++ komutlar/küfür-engel.js
@@ -1,54 +1,35 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
-const ayarlar = require('../ayarlar.json')
-let prefix = ayarlar.prefix
-//lrowsxrd
-exports.run = async (client ,message, args) => {
-  //lrowsxrd
-if(args[0] === 'aç') {
-  //lrowsxrd
-db.set(`${message.guild.id}.kufur`, true)
-  //lrowsxrd
-const kinda = new Discord.MessageEmbed()  
-  //lrowsxrd
-.setDescription(`Küfür Engel Başarılı Bir Şekilde Akif Edildi!`)
-.setColor("GREEN")
-//lrowsxrd
-return message.channel.send(kinda)
+//takashiakira tarafından yazılmıştır. Başka yerlerde paylaşılması yasaktır!
+
+const db = ('quick.db')
+const Discord = ('discord.js')
+const ayarlar = require('../ayarlar.json');
+
+exports.run = async (bot, message, args) => {
+    if (!args[0]) return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(`Lütfen *Aç* veya *Kapat* diye belirtiniz. ${ayarlar.prefix}küfürengel aç `).setColor("RED"));
+    if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('Bu komutu kullanabilmek için `Sunucuyu Yönet` Yetkisine sahip olmalısınız')
+
+    if(args[0] == 'aç') {
+        db.set(`kufur_${message.guild.id}`, 'acik').then(i =>{
+            return message.channel.sendEmbed(new Discord.RichEmbed().setDescription('Küfür engel başarıyla açılmıştır `Üyeleri Yasakla` yetkisine sahip olanların küfürü engellenmeyecektir').setColor('RANDOM'));
+        })
+    }
+    if (args[0] == 'kapat'){
+        db.set(`kufur_$(messsage.guild.id)`, 'kapali').then(i =>{
+            return message.channel.sendEmbed(new Discord.RichEmbed().setDescription('Küfür engel başarıyla kapatılmıştır.').setColor('RANDOM'));
+        })
+    }
 }
-  
-if (args[0] === 'kapat') {
-  //lrowsxrd
-db.delete(`${message.guild.id}.kufur`)
-  
-const kinda = new Discord.MessageEmbed() 
-  //lrowsxrd
-.setDescription(`Küfür Engel Başarılı Bir Şekilde Deaktif Edildi!`)
-.setColor("GREEN")
-  
-return message.channel.send(kinda)
-}
-{
-  //lrowsxrd
-const kinda = new Discord.MessageEmbed() 
-  
-.setDescription('Lütfen **aç** Veya **kapat** Yazın. Örnek Kullanım : **${prefix}küfür-engel aç/kapat**')
-.setColor("RED")
-//lrowsxrd
-return message.channel.send(kinda)
-}
-  //lrowsxrd
-};
 
 exports.conf = {
- enabled: true,
- guildOnly: false,
- aliases: ['küfürengel'], 
- permLevel: 0
+    enabled: true,
+    guildOnly: false,
+    aliases: ['küfür-engel'],
+    kategori: "moderasyon",
+    permLevel: 3
 };
-//lrowsxrd
+
 exports.help = {
- name: 'küfür-engel',
- description: 'lrows v12',
- usage: 'küfür-engel'
+    name: 'küfürengel',
+    description: 'Küfür engelleme sistemini açıp/kapatır',
+    usage: 'küfür-engelleme'
 };
