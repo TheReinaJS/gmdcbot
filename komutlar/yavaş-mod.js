a194ff51a57004727f4be76c528e53a5c807b24d
Glitch (nostalgic-aboard-bedbug) <none>
2021-05-18 16:38:51 +0000

    🏛💏 Checkpoint ./events/ready.js:31862299/83 ./komutlar/yavaş-mod.js:31862299/332

--- komutlar/yavaş-mod.js
+++ komutlar/yavaş-mod.js
@@ -1,28 +1,26 @@
-const emran = require('discord.js');
-//codarev12
+const wolfs = require('discord.js');
 
 exports.run = async(client, message, args) => {
-              const codare = require('../ayarlar.json')
-            let prefix = codare.prefix
-//codarev12
+              const wolf = require('../ayarlar.json')
+            let prefix = wolf.prefix
+
 
               if(message.channel.type == "dm")  return;
 if (message.channel.type !== "text") return;
     if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.reply("Bu Komutu Kullanabilmen İçin **Mesajları Yönet** Yetkisine Sahip Olman Gerek!.");  
-//codarev12
-const limit = args[0] ? args[0] : 0;//codare
-  if(!limit) {//codare  //sharpen
-              var embed = new emran.MessageEmbed()
-                .setDescription(`Doğru kullanım: \`${prefix}yavaş-mod [0/100]\``)//codare
-                .setColor('RANDOM')
-                .setTimestamp()
+
+const limit = args[0] ? args[0] : 0;
+  if(!limit) {
+              var embed = new wolfs.MessageEmbed()
+                .setDescription(`**<:841036035688497152:844213748134838313> Doğru kullanım ・ \`${prefix}yavaş-mod [0/100]\`**`)
+                .setColor('#7289DA')
             message.channel.send({embed})
             return
-          }//codare
+          }
 if (limit > 100) {
-    return message.channel.send(new emran.MessageEmbed().setDescription("Süre limiti maksimum **100** saniye olabilir.").setColor('RED'));
+    return message.channel.send(new wolfs.MessageEmbed().setDescription("Süre limiti maksimum **100** saniye olabilir.").setColor('RED'));
 }  
-    message.channel.send(new emran.MessageEmbed().setDescription(`Yazma süre limiti **${limit}** saniye olarak ayarlanmıştır.`).setColor('GREEN')).then(m => m.delete({ timeout: 5000, reason: 'Siliniyor.' }));
+    message.channel.send(new wolfs.MessageEmbed().setDescription(`Yazma süre limiti **${limit}** saniye olarak ayarlanmıştır.`).setColor('GREEN')).then(m => m.delete({ timeout: 5000, reason: 'Siliniyor.' }));
   
 var request = require('request');
 request({
